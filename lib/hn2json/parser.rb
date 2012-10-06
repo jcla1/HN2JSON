84097f793ebd8c7dcdfab6d4b4d8308dd2698c7c
Joseph Adams <whitegolem@gmail.com>
2012-10-06 10:36:44 +0200

    Put boiletplate code for extrcting attrs in private functions

--- lib/hn2json/parser.rb
+++ lib/hn2json/parser.rb
@@ -50,20 +50,11 @@ module HN2JSON
 
       comhead = @doc.css('.comhead')[0]
       
-      date_regex = /.*\s(.*\s.*\sago)/
-      ago = date_regex.match(comhead.content)[1]
-      date_posted = Chronic.parse(ago).to_s
+      date_posted = get_date comhead
 
-      posted_by = comhead.css('a')[0].content
+      posted_by = get_posted_by comhead
 
-      comments = []
-
-      full_comments = @doc.css('td > img[width="0"]').xpath("..").xpath("..").css('.default')
-
-      full_comments.each do |comment|
-        comment_id = comment.css('span a')[1]['href'].gsub("item?id=", '')
-        comments.push comment_id
-      end
+      comments = get_comments
 
       entity.add_attrs do |e|
         e.parent = parent
@@ -79,22 +70,13 @@ module HN2JSON
 
       subtext = @doc.css('.subtext')[0]
 
-      date_regex = /.*\s(.*\s.*\sago)/
-      ago = date_regex.match(subtext.content)[1]
-      date_posted = Chronic.parse(ago).to_s
+      date_posted = get_date subtext
 
-      posted_by = subtext.css('a')[0].content
+      posted_by = get_posted_by subtext
 
-      votes = subtext.css('span')[0].content.to_i
+      votes = get_num_votes subtext
 
-      comments = []
-
-      full_comments = @doc.css('td > img[width="0"]').xpath("..").xpath("..").css('.default')
-
-      full_comments.each do |comment|
-        comment_id = comment.css('span a')[1]['href'].gsub("item?id=", '')
-        comments.push comment_id
-      end
+      comments = get_comments
 
       head = @doc.css('.title a')[0]
 
@@ -118,36 +100,19 @@ module HN2JSON
 
       td = @doc.css('td')[10]
 
-      if td.css('table').length > 0
-        fulltext = ''
-        voting_on = voting_on_from_table td.css('table')[0]
-      else
-        fulltext = td.content
-        voting_on = voting_on_from_table @doc.css('td')[12].css('table')[0]
-      end
-
       subtext = @doc.css('.subtext')[0]
 
-      date_regex = /.*\s(.*\s.*\sago)/
-      ago = date_regex.match(subtext.content)[1]
-      date_posted = Chronic.parse(ago).to_s
+      date_posted = get_date subtext
 
-      posted_by = subtext.css('a')[0].content
+      posted_by = get_posted_by subtext
 
-      votes = subtext.css('span')[0].content.to_i
+      votes = get_num_votes subtext
 
-      comments = []
-
-      full_comments = @doc.css('td > img[width="0"]').xpath("..").xpath("..").css('.default')
-
-      full_comments.each do |comment|
-        comment_id = comment.css('span a')[1]['href'].gsub("item?id=", '')
-        comments.push comment_id
-      end
+      comments = get_comments
 
       entity.add_attrs do |e|
         e.title = title
-        e.fulltext = fulltext
+        #e.fulltext = fulltext
         e.date_posted = date_posted
         e.posted_by = posted_by
         e.votes = votes
@@ -164,22 +129,13 @@ module HN2JSON
 
       subtext = @doc.css('.subtext')[0]
 
-      date_regex = /.*\s(.*\s.*\sago)/
-      ago = date_regex.match(subtext.content)[1]
-      date_posted = Chronic.parse(ago).to_s
+      date_posted = get_date subtext      
 
-      posted_by = subtext.css('a')[0].content
+      posted_by = get_posted_by subtext
 
-      votes = subtext.css('span')[0].content.to_i
+      votes = get_num_votes subtext
 
-      comments = []
-
-      full_comments = @doc.css('td > img[width="0"]').xpath("..").xpath("..").css('.default')
-
-      full_comments.each do |comment|
-        comment_id = comment.css('span a')[1]['href'].gsub("item?id=", '')
-        comments.push comment_id
-      end
+      comments = get_comments
 
       entity.add_attrs do |e|
         e.title = title
@@ -191,29 +147,48 @@ module HN2JSON
       end
     end
 
-    def voting_on_from_table table
-      trs = table.css('tr')
 
-      voting_on = []
+    private
 
-      (trs.length / 3).times do 
-        voting_on.push []
+    def get_voting_on table
+    end
+
+    def get_comments
+      comments = []
+
+      full_comments = @doc.css('td > img[width="0"]').xpath("..").xpath("..").css('.default')
+
+      full_comments.each do |comment|
+        comment_id = comment.css('span a')[1]['href'].gsub("item?id=", '')
+        comments.push comment_id
       end
 
-      i = 0
-      while i <= trs.length
-        if i + 1 % 3 != 0
-          if i % 2 == 0
-            puts i % 3
-            voting_on[(i - 1) % 3].push trs[i].css('.comment > div > font')[0].content
-          else
-            voting_on[i % 3].push trs[i].css('.default > .comhead > span')[0].content.gsub(/\spoints?/, '')
-          end
-        end
-        i += 1
-      end
+      # TODO: Follow the [More] link
+      #
+      # $tr = $('tr')
+      # $($tr[$tr.length - 3]).find('a').eq(0).attr('href')
 
-      return []
+      #trs = @doc.css('tr')
+
+      #more_url = trs[trs.length - 3].css('a')[0]['href']
+
+      return comments
+    end
+
+    def get_num_votes subtext
+      return subtext.css('span')[0].content.to_i
+    end
+
+    def get_posted_by subtext
+      return subtext.css('a')[0].content
+    end
+
+    def get_date subtext
+      date_regex = /.*\s(.*\s.*\sago)/
+      ago = date_regex.match(subtext.content)[1]
+      date_posted = Chronic.parse(ago).to_s
+
+      return date_posted
     end
 
   end
